379ecb5e9cb8593ff5e9bbbbb57961263420e66e
Darren <valovcin@physics.ucsb.edu>
2015-05-26 07:09:25 -0700

    StartV2

--- __main__.py
+++ __main__.py
@@ -9,12 +9,13 @@ from PyQt4 import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
 import threading
-from mainWindow_ui import Ui_MainWindow
+from mainWindowV2_ui import Ui_MainWindow
 import datetime
 import re
 import time
-from Oscilloscope import TDSOscilloscope
-from KeithleyInstruments import Keithley2400
+from InstsAndQt.Instruments import Agilent6000, Keithley2400Instr
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 
 
 class Win(QtGui.QMainWindow):
@@ -25,8 +26,6 @@ class Win(QtGui.QMainWindow):
 
     def __init__(self):
         super(Win,self).__init__()
-        self.scope = TDSOscilloscope(GPIB_Number = 'GPIB1::1::INSTR')
-        self.keith = Keithley2400(GPIB_Number = 'GPIB1::24::INSTR')
         self.initUI()
         
         self.runFlag = False
@@ -35,7 +34,7 @@ class Win(QtGui.QMainWindow):
         self.oscSig.connect(self.updateOsc)
         self.statusSig.connect(self.updateStatusBar)
         self.intSig.connect(self.updateIntegration)
-        self.changeChannel(0) #set the first channel
+        # self.changeChannel(0) #set the first channel
         
         self.leakValues = np.empty((0,2))
         self.intValues = np.empty((0,2))
@@ -54,31 +53,59 @@ class Win(QtGui.QMainWindow):
         plotitem.setLabel('top',text='Leakage Current')
         plotitem.setLabel('left',text='Current',units='A')
         plotitem.setLabel('bottom',text='voltage', units='V')
-        self.oscGraph = self.ui.gOscilloscope.plot()
-        plotitem = self.ui.gOscilloscope.getPlotItem()
-        plotitem.setLabel('top',text='Waveform')
-        plotitem.setLabel('left',text='Voltage',units='V')
-        plotitem.setLabel('bottom',text='Time', units='s')
+        # self.oscGraph = self.ui.gOscilloscope.plot()
+        # plotitem = self.ui.gOscilloscope.getPlotItem()
+        # plotitem.setLabel('top',text='Waveform')
+        # plotitem.setLabel('left',text='Voltage',units='V')
+        # plotitem.setLabel('bottom',text='Time', units='s')
+
+        self.ui.gOscilloscope.setXLabel("Time", "s")
+        self.ui.gOscilloscope.setY1Label("Signal", "V")
+        self.ui.gOscilloscope.setY2Label("Ref", "V")
+
+        bgCol = pg.mkBrush(QtGui.QColor(255, 0, 0, 50))
+        sgCol = pg.mkBrush(QtGui.QColor(0, 255, 0, 50))
+        self.lrSig = pg.LinearRegionItem(brush = sgCol)
+        self.lrBg  = pg.LinearRegionItem(brush = bgCol)
+        self.ui.gOscilloscope.plotItem1.addItem(self.lrSig)
+        self.ui.gOscilloscope.plotItem1.addItem(self.lrBg)
+
+
+
         self.intGraph = self.ui.gValueVsV.plot()
         plotitem = self.ui.gValueVsV.getPlotItem()
         plotitem.setLabel('top',text='Waveform')
         plotitem.setLabel('left',text='Integrated Scope',units='V')
         plotitem.setLabel('bottom',text='Gate Voltage', units='V')
-#        self.integrationRegion = pg.LinearRegionItem([0, 1],
-#                                                     bounds=[0, 1],
-#                                                     brush=pg.mkBrush(QtGui.QColor(0,255,0,50)))
-#        self.integrationRegion.setMovable(False)
-#        self.ui.gOscilloscope.addItem(self.integrationRegion)
+
+        try:
+            import visa
+            rm = visa.ResourceManager()
+            gpibList = [i.encode('ascii') for i in rm.list_resources()]
+        except Exception as e:
+            print "VISA Error:", e
+            gpibList = ['a', 'b', 'c']
+
+
+        # Add the items to the list and make them fake
+        gpibList.append("Fake")
+        self.ui.cOscGPIB.addItems(gpibList)
+        self.ui.cOscGPIB.setCurrentIndex(len(gpibList)-1)
+        self.ui.cKeithGPIB.addItems(gpibList)
+        self.ui.cKeithGPIB.setCurrentIndex(len(gpibList)-1)
+
+        self.ui.cOscGPIB.currentIndexChanged.connect(self.openOsc)
+        self.ui.cKeithGPIB.currentIndexChanged.connect(self.openKeith)
+
+
                 
         #Connect all the interfaces
         self.ui.bStartScan.clicked.connect(self.startScan)
         self.ui.bAbortScan.clicked.connect(self.abortScan)
         self.ui.bAbortScan.setEnabled(False)
-        self.ui.mFileExit.triggered.connect(self.close)
         self.ui.cChannelPicker.currentIndexChanged.connect(self.changeChannel)
-        self.ui.mFileUpdate.triggered.connect(self.getScope)
         self.ui.bSaveDirectory.clicked.connect(self.updateSaveLoc)
-        self.ui.bResetLeak.clicked.connect(self.resetLeak)
+        # self.ui.bResetLeak.clicked.connect(self.resetLeak)
         
 #        self.ui.tIntEnd.editingFinished.connect(self.updateLR)
 #        self.ui.tIntStart.editingFinished.connect(self.updateLR)
@@ -87,6 +114,54 @@ class Win(QtGui.QMainWindow):
         
 #    def updateLR(self):
 #        self.integrationRegion.setBounds([float(self.ui.tIntStart.text()), float(self.ui.tIntEnd.text())])
+
+    def openOsc(self, GPIB = None):
+        if GPIB is None:
+            GPIB = str(self.ui.cOscGPIB.currentText())
+        try:
+            self.scope.close()
+        except:
+            pass
+
+        # Disconnect to prevent recursions when you redo this
+        self.ui.cOscGPIB.currentIndexChanged.disconnect(self.openOsc)
+        try:
+            self.scope = Agilent6000(GPIB)
+            newIdx = self.ui.cOscGPIB.findText(GPIB)
+            if newIdx == -1: # Error finding it
+                print "ERROR UPDATING OSC GPIB, wanted", GPIB
+            else:
+                self.ui.cOscGPIB.setCurrentIndex(newIdx)
+        except Exception as e:
+            print "Error opening Osc"
+            self.scope = Agilent6000("Fake")
+            self.ui.cOscGPIB.setCurrentIndex(self.ui.cOscGPIB.count()-1)
+
+        self.ui.cOscGPIB.currentIndexChanged.connect(self.openOsc)
+
+    def openKeith(self, GPIB = None):
+        if GPIB is None:
+            GPIB = str(self.ui.cKeithGPIB.currentText())
+        try:
+            self.keith.close()
+        except:
+            pass
+
+        # Disconnect to prevent recursions when you redo this
+        self.ui.cKeithGPIB.currentIndexChanged.disconnect(self.openKeith)
+        try:
+            self.keith = Keithley2400Instr(GPIB)
+            newIdx = self.ui.cKeithGPIB.findText(GPIB)
+            if newIdx == -1: # Error finding it
+                print "ERROR UPDATING KETIH GPIB, wanted", GPIB
+            else:
+                self.ui.cKeithGPIB.setCurrentIndex(newIdx)
+        except Exception as e:
+            print "Error opening KETIH"
+            self.keith = Keithley2400Instr("Fake")
+            self.ui.cKeithGPIB.setCurrentIndex(self.ui.cKeithGPIB.count()-1)
+
+        self.ui.cOscGPIB.currentIndexChanged.connect(self.openKeith)
         
     def getScope(self):
         self.scope.getScopeValues()
@@ -97,25 +172,10 @@ class Win(QtGui.QMainWindow):
 
     def startScan(self):
         #test inputs
-        try:
-            start = self.parseInp(self.ui.tGateStart.text())
-            self.ui.tGateStart.setText(str(start))
-            step = self.parseInp(self.ui.tGateStep.text())
-            self.ui.tGateStep.setText(str(step))
-            end = self.parseInp(self.ui.tGateEnd.text())
-            self.ui.tGateEnd.setText(str(end))
-            m = self.parseInp(self.ui.tMeasureEvery.text())
-            self.measureEvery = int(m)
-        except Exception as e:
-            self.ui.statusbar.showMessage('Error converting input: {}'.format(e[1]), 3000)
-            return
-        if (end-start)*step<0:
-            self.ui.statusbar.showMessage('Incorrect step size', 3000)
-            return
-#        if start*end<0:
-#            self.ui.statusbar.showMessage('Sorry. Put start/stop on same sign', 3000)
-#            return
-        #disable all the buttons and inputs so they don't get changed and confuse the user
+        start = self.ui.tGateStart.value()
+        step = self.ui.tGateStep.value()
+        end = self.ui.tGateEnd.value()
+
         self.ui.cChannelPicker.setEnabled(False)
         self.ui.bStartScan.setEnabled(False)
         self.ui.bAbortScan.setEnabled(True)
@@ -147,115 +207,99 @@ class Win(QtGui.QMainWindow):
         try:
             self.statusSig.emit('Aborting...')
             self.scanningThread.join()
-        except:
-            pass
-    
-    def parseInp(self, inp):
-        ret = None
-        #see if we can just turn it into a number and leave if we can
-        try:
-            ret = float(inp)
-            return ret
-        except:
-            pass
-        #tests to see whether digit is whole number or decimal, and if it has 
-        #some modifier at the end
-        toMatch = re.compile('-?(\d+\.?\d*|\d*\.\d+)(m|u|n)?\Z')
-        if re.match(toMatch, inp):
-            convDict = {'m': 1e-3, 'u':1e-6, 'n':1e-9}
-            try:
-                ret = (float(inp[:-1]) * #convert first part to number
-                   convDict[[b for b in convDict.keys() if b in inp][0]]) #and multiply by the exponential
-                return ret
-            except:
-                print 'uh oh'
-        else:
-            raise TypeError('Error with input', str(inp))
+        except Exception as e:
+            print "error in abortion,", e
             
     def runScan(self, *args):
         start = args[0]
         step = args[1]
         end = args[2]
-        voltageRange = np.arange(start, end, step)
-        voltageRange = np.append(voltageRange, end)
-        
-        self.keith.turnOn()
-        #Incase things got changed.
-        self.scope.getScopeValues()
-        
-        if not start == 0:
-            print 'not equal'
-            startStep = step
-            if start*step<0:
-                startStep = -step
-            for voltage in np.arange(0, start, startStep):
-                if not self.runFlag:
-                    break
-                self.doRamp(voltage)
-        else:
-            print 'equal'
-            
-        measureCount = self.measureEvery
-        
-        for voltage in voltageRange:
-            #Make sure the main thread didn't abort the run
-            if not self.runFlag:
-                break
-            measureCount = measureCount - 1
-            if measureCount == 0 or voltage==voltageRange[0]:
-                measureCount = self.measureEvery
-                if not self.doMeasurementPoint(voltage):
-                    print 'Error: Time out?'
-                    continue
-            else:
-                self.doRamp(voltage)
-                    
-        
-        ###########
-        #Uncomment this loop if you want to measure for hysteresis
-#        for voltage in voltageRange[::-1]:
-#            #Make sure the main thread didn't abort the run
-#            if not self.runFlag:
-#                break
-#            if not self.doMeasurementPoint(voltage, True):
-#                break
-        
-        
-        for voltage in np.arange(self.voltage, 0, -abs(step)*np.sign(self.voltage)):
-            self.doRamp(voltage)
-        
-        
-            
-        self.keith.setVoltage(0)
-        self.keith.turnOff()
+
+        try:
+            self.keith.doLoop(start, end, step, toCall=self.doMeasurementPoint)
+        except AssertionError:
+            print "Error: Invalid scan parameters"
+            self.abortScan()
+            return
+        except Exception as E:
+            print "Error running loop", E
+            self.abortScan()
+            return
+
         self.saveLeakage()
         self.abortScan()
-        
-    def doRamp(self, voltage):
-        self.voltage = voltage
-        self.keith.setVoltage(voltage)
-        time.sleep(.2)
-        self.statusSig.emit('Ramping. Voltage: '+str(voltage))
+
+        # self.keith.turnOn()
+        # #Incase things got changed.
+        # self.scope.getScopeValues()
+        #
+        # if not start == 0:
+        #     print 'not equal'
+        #     startStep = step
+        #     if start*step<0:
+        #         startStep = -step
+        #     for voltage in np.arange(0, start, startStep):
+        #         if not self.runFlag:
+        #             break
+        #         self.doRamp(voltage)
+        # else:
+        #     print 'equal'
+        #
+        # measureCount = self.measureEvery
+        #
+        # for voltage in voltageRange:
+        #     #Make sure the main thread didn't abort the run
+        #     if not self.runFlag:
+        #         break
+        #     measureCount = measureCount - 1
+        #     if measureCount == 0 or voltage==voltageRange[0]:
+        #         measureCount = self.measureEvery
+        #         if not self.doMeasurementPoint(voltage):
+        #             print 'Error: Time out?'
+        #             continue
+        #     else:
+        #         self.doRamp(voltage)
+        #
+        # for voltage in np.arange(self.voltage, 0, -abs(step)*np.sign(self.voltage)):
+        #     self.doRamp(voltage)
+        #
+        #
+        #
+        # self.keith.setVoltage(0)
+        # self.keith.turnOff()
+        # self.saveLeakage()
+        # self.abortScan()
         
     def doMeasurementPoint(self, voltage, reverse = False):
         self.voltage = voltage
         self.statusSig.emit('Measuring. Voltage: '+str(voltage))
-        self.keith.setVoltage(voltage)
-        time.sleep(.2) #Sleep incase there's a lag between updating the scope
-        #Issues with the scope timing out, so test for it
-        self.scope.start_acquire()
-        try:
-            self.scope.acq_complete()
-        except:
-            self.statusSig.emit('Error reading from scope (other timeout?)')
-            return False
-        try:
-            self.scope.read_channel(self.oscChannel)
-        except:
-            self.statusSig.emit('Error reading from scope (timeout?)')
-            return False
-        
-        self.getScope()
+        # self.keith.setVoltage(voltage)
+        # time.sleep(.2) #Sleep incase there's a lag between updating the scope
+        # #Issues with the scope timing out, so test for it
+        # self.scope.start_acquire()
+        # try:
+        #     self.scope.acq_complete()
+        # except:
+        #     self.statusSig.emit('Error reading from scope (other timeout?)')
+        #     return False
+        # try:
+        #     self.scope.read_channel(self.oscChannel)
+        # except:
+        #     self.statusSig.emit('Error reading from scope (timeout?)')
+        #     return False
+        #
+        # self.getScope()
+
+        Sig, Ref = self.scope.getMultipleChannels(
+            str(self.ui.cChannelPicker.currentText())[-1],
+            str(self.ui.cChannelPickerRef.currentText())[-1]
+        )
+
+        sigSigBC = self.scope.integrateData(Sig, self.lrSig.getRegion())
+        sigBgBC = self.scope.integrateData(Sig, self.lrBg.getRegion())
+        refSigBC = self.scope.integrateData(Ref, self.lrSig.getRegion())
+        refBgBC = self.scope.integrateData(Ref, self.lrBg.getRegion())
+
         self.scopeValues = self.scope.scaleWaveforms()
 #            self.scopeValues = self.scope.wfmDict[self.oscChannel]
         self.time = np.array(range(len(self.scopeValues))) * self.scope.dt
